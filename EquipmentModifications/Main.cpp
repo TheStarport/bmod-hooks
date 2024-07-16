3fb6d6b3a0f8c99b9be5722e0a36f299a2355491
Rupert Walker <RupertCW@gmail.com>
2024-07-16 14:52:34 +0100

    Update Main.cpp

--- EquipmentModifications/Main.cpp
+++ EquipmentModifications/Main.cpp
@@ -9,6 +9,58 @@ GlobalTimeFunc globalTimingFunction;
 constexpr double SixtyFramesPerSecond = 1.0 / 60.0;
 double timeCounter;
 
+
+
+typedef int(__cdecl* ThornDoBuffer)(char* str, int length, char* desc);
+typedef int(__cdecl* ThornDoMain)(void* state, int bin);
+static ThornDoBuffer LuaDoBuffer = ThornDoBuffer(0x06F4CCE0);
+static ThornDoMain LuaDoMain = ThornDoMain(0x6F4CAE0);
+unsigned char* thornDoMainData;
+
+int LuaDoString(char* str)
+{
+	return LuaDoBuffer(str, strlen(str), str);
+}
+
+static int LuaDoMainHook(void* state, int bin)
+{
+	Utils::Memory::UnDetour((unsigned char*)LuaDoMain, thornDoMainData);
+
+	Archetype::Ship* ship = Utils::GetShipArch();
+	std::stringstream ss;
+	// Add any information to the ship object for use in Lua code that you want
+	if (ship)
+	{
+		ss << "SHIP = { " <<
+			"class = " << ship->iShipClass << ", " <<
+			"cargo = " << ship->fHoldSize << ", " <<
+			"model = \"" << ship->szName << "\", " <<
+			"arch_type = " << ship->iArchType << ", " <<
+			"id = " << ship->iArchID << ", " <<
+			"mass = " << ship->fMass << ", " <<
+			"hitpoints = " << ship->fHitPoints <<
+			"} ";
+	}
+
+	LuaDoString((char*)ss.str().c_str());
+	auto res = LuaDoMain(state, bin);
+	Utils::Memory::Detour((unsigned char*)LuaDoMain, LuaDoMainHook, thornDoMainData);
+	return res;
+}
+
+using ScriptLoadPtr = void* (*)(const char*);
+ScriptLoadPtr thornLoad;
+unsigned char* thornLoadData;
+
+void* ScriptLoadHook(const char* script)
+{
+	thornDoMainData = PBYTE(malloc(5));
+	Utils::Memory::UnDetour(PBYTE(thornLoad), thornLoadData);
+	Utils::Memory::Detour((unsigned char*)LuaDoMain, LuaDoMainHook, thornDoMainData);
+	return thornLoad(script);
+}
+
+
 // Called every frame
 void __cdecl Update(double delta)
 {
@@ -18,7 +70,7 @@ void __cdecl Update(double delta)
 		UpdateQueuedShots(delta);
 		timeCounter -= SixtyFramesPerSecond;
 	}
-	
+
 
 	Utils::Memory::UnDetour((unsigned char*)globalTimingFunction, data);
 	globalTimingFunction(delta);
@@ -47,6 +99,10 @@ void SetupHack()
 	SetupClassExpansion();
 	InitInfocardEdits();
 	FreelancerOnlyHacks();
+
+	thornLoadData = (unsigned char*)malloc(5);
+	thornLoad = reinterpret_cast<ScriptLoadPtr>(GetProcAddress(GetModuleHandleA("common.dll"), "?ThornScriptLoad@@YAPAUIScriptEngine@@PBD@Z")); // NOLINT
+	Utils::Memory::Detour(PBYTE(thornLoad), ScriptLoadHook, thornLoadData);
 
 }
 
